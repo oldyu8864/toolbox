ea1722ded6fc127e0f5a7db84033b0751c28bc46
YuJunquan <993669609@qq.com>
2019-04-16 19:27:10 +0800

    oldyu0002-添加了一个stringutils项目模块

---
+++ stringutils/src/main/java/com/oldyu/stringutils/StringutilsApplication.java
@@ -0,0 +1,13 @@
+package com.oldyu.stringutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringutilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringutilsApplication.class, args);
+    }
+
+}
